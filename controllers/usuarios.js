cd66d1f9bb885a453dd7d3a004de1ea8e29c9865
Miguel Lopez <ing.miguellv@gmail.com>
2023-01-04 13:14:21 -0600

    agrego express validator para nombre, correo, password y rol

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,5 @@
 const { response, request } = require('express')
+const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario')
 
 const usuariosGet = (req = request, res = response) => {
@@ -13,8 +14,18 @@ const usuariosGet = (req = request, res = response) => {
 }
 
 const usuariosPost = async(req, res) => {
-  const body = req.body
-  const usuario = new Usuario(body)
+  const { nombre, correo, password, role } = req.body
+  const usuario = new Usuario({ nombre, correo, password, role })
+
+  const existeEmail = await Usuario.findOne({ correo })
+  if ( existeEmail) {
+    return res.status(400).json({
+      msg: 'El correo ya está registrado'
+    })
+  }
+
+  const salt = bcryptjs.genSaltSync()
+  usuario.password = bcryptjs.hashSync( password, salt )
 
   await usuario.save()
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,4 +1,6 @@
 const { Router } = require('express')
+const { check } = require('express-validator')
+const { validarCampos } = require('../middlewares/validar-campos')
 const {
   usuariosGet,
   usuariosPost,
@@ -10,7 +12,13 @@ const {
 const router = Router()
 
 router.get('/', usuariosGet)
-router.post('/', usuariosPost)
+router.post('/', [
+  check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+  check('correo', 'Correo no válido').isEmail(),
+  check('password', 'El password debe contener más de 6 caracteres').isLength({ min: 6 }),
+  check('role', 'No es un rol permitido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+  validarCampos
+], usuariosPost)
 router.put('/:id', usuariosPut)
 router.patch('/', usuariosPatch)
 router.delete('/', usuariosDelete)
